b224e2cb44a0739fe6d19b15032ddbd1e8c16a51
Habtamu <habtamuwolde92@gmail.com>
2023-01-29 21:55:34 +0530

    full responsive comming soon webpage

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const img = document.querySelector('.hero-image')
+const form = document.querySelector('form')
 const mql = window.matchMedia('(min-width: 500px)')
 function changeHeroImage(){
     if(mql.matches){
@@ -9,5 +10,23 @@ function changeHeroImage(){
     }
 }
 
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const input = document.querySelector('.user-input')
+    const regex = /\@/gi
+    const errorImage = document.querySelector('.error-image')
+    const errorMessage =document.querySelector('.error-message')
+    if(input.value && regex.test(input.value)){
+       form.reset()
+       errorImage.classList.add('none')
+       errorMessage.textContent = 'Success'
+    }
+    else{
+        errorMessage.textContent = 'please provide valid email'
+        errorImage.classList.remove('none')
+    }
+})
+
+
 changeHeroImage()
-mql.addEventListener('resize', changeHeroImage)
+mql.addListener(changeHeroImage)
